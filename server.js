c3cf03e571195c780a9e5d787661616629d58806
jvcataquiz <jvcataquiz@gmail.com>
2022-11-13 14:52:19 +0800

    initialized backend- express

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const Data = require('./data.json');
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) =>{
+res.send(Data)
+})
+
+const categories = ['fruit', 'vegetable', 'dairy'];
+
+app.get('/new', (req, res) => {
+    res.send(categories);
+})
+
+app.listen(8080, () => {
+    console.log("listening on port 8080");
+})
